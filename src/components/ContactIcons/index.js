35a99197093f799dcb3c9117a6862663deb16eb5
NOat <nicolas.cedano.avena@gmail.com>
2021-08-17 00:14:39 -0500

    footer index

--- src/components/ContactIcons/index.js
+++ src/components/ContactIcons/index.js
@@ -25,7 +25,21 @@ function ContactIcons() {
         },
     ]
 
-
+    return (
+        <div className="mt-2 mt-lg-0">
+            {contactLinks.map((contactLink) => {
+                return (
+                    <a href={contactLink.url}
+                        key={contactLink.name}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="m-3">
+                        <span><i className={contactLink.classes}></i></span>
+                    </a>
+                )
+            })}
+        </div>
+    )
 }
 
 export default ContactIcons;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+//IMPORT COMPONENTS
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ContactIcons from '../ContactIcons'
+
+
+
+
+function Footer() {
+    return (
+        <footer className="bg-light p-3 shadow footer">
+            <Container>
+                <Row>
+                    <Col sm={12} md={6} className="m-md-0">
+                        <p className="m-md-0 copyright">© 2021 Nicolas Avena</p>
+                    </Col>
+                    <Col sm={12} md={6} className="d-flex justify-content-md-end justify-content-center">
+                        <ContactIcons></ContactIcons>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer;
