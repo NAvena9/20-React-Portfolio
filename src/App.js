d653535bd51de8e09a7d43a358d3c40179103977
NOat <nicolas.cedano.avena@gmail.com>
2021-08-18 08:16:51 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App(){
         <Header/>
         <main>
           <Switch>
-          <Route exact path="/" component={About} />
+            <Route exact path="/20-React-Portfolio" component={About} />
             <Route exact path="/about" component={About} />
             <Route exact path="/projects" component={Projects} />
             <Route exact path="/resume" component={Resume} />
